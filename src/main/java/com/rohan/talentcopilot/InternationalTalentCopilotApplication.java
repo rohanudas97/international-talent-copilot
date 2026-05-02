ebe22b5f25f004abed3dc27c1842e63d6b69a75d
Rohan & Suraj <Rohanudas97@gmail.com>
2026-05-02 16:45:56 -0400

    Day 1: setup Spring Boot app with health endpoint

---
+++ src/main/java/com/rohan/talentcopilot/InternationalTalentCopilotApplication.java
@@ -0,0 +1,13 @@
+package com.rohan.talentcopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternationalTalentCopilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternationalTalentCopilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohan/talentcopilot/InternationalTalentCopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.talentcopilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternationalTalentCopilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
